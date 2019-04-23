6fa63d2d4d97873904fd50da3ddbaa86a275320f
Alexander Fisher <alex@linfratech.co.uk>
2019-04-23 11:02:11 +0100

    Rubocop Autofix: Style/SignalException

--- lib/hiera/backend/eyaml/encryptors/gpg.rb
+++ lib/hiera/backend/eyaml/encryptors/gpg.rb
@@ -4,7 +4,7 @@ rescue LoadError
   begin
     require 'ruby_gpg'
   rescue LoadError
-    fail "hiera-eyaml-gpg requires either the 'gpgme' or 'ruby_gpg' gem"
+    raise "hiera-eyaml-gpg requires either the 'gpgme' or 'ruby_gpg' gem"
   end
   require 'hiera/backend/eyaml/encryptors/gpg/puppet_gpg'
 end
