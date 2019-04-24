225b706bee7c155638313b59ef9a46912dd82855
Alexander Fisher <alex@linfratech.co.uk>
2019-04-24 11:23:47 +0100

    Make `find_recipients` private

--- lib/hiera/backend/eyaml/encryptors/gpg.rb
+++ lib/hiera/backend/eyaml/encryptors/gpg.rb
@@ -72,30 +72,6 @@ class Hiera
             end
           end
 
-          def self.find_recipients
-            recipient_option = option :recipients
-
-            unless recipient_option.nil?
-              debug('Using --recipients option')
-              return recipient_option.split(',')
-            end
-
-            recipient_file_option = option :recipients_file
-            recipient_file = if recipient_file_option.nil?
-                               debug('Searching for any hiera-eyaml-gpg.recipients files in path')
-                               find_recipient_file
-                             else
-                               debug('Using --recipients-file option')
-                               Pathname.new(recipient_file_option)
-                             end
-
-            return [] if recipient_file.nil?
-
-            recipient_file.readlines.map do |line|
-              line.strip unless line.start_with?('#') || line.strip.empty?
-            end.compact
-          end
-
           def self.encrypt(plaintext)
             unless defined?(GPGME)
               raise RecoverableError, "Encryption is only supported when using the 'gpgme' gem"
@@ -181,27 +157,53 @@ class Hiera
             STDERR.puts 'The GPG encryptor does not support creation of keys, use the GPG command lines tools instead'
           end
 
-          private
+          class << self
+            private
 
-          def self.find_recipient_file
-             # if we are editing a file, look for a hiera-eyaml-gpg.recipients file
-             filename = case Eyaml::Options[:source]
-                        when :file
-                          Eyaml::Options[:file]
-                        when :eyaml
-                          Eyaml::Options[:eyaml]
-                        end
+            def find_recipients
+              recipient_option = option :recipients
 
-             return if filename.nil?
+              unless recipient_option.nil?
+                debug('Using --recipients option')
+                return recipient_option.split(',')
+              end
 
-             root = Pathname.new(filename).realpath.dirname
-             selected_file = nil
-             root.descend do |path|
-               potential_file = path.join('hiera-eyaml-gpg.recipients')
-               selected_file = potential_file if potential_file.exist?
-             end
-             debug("Using file at #{selected_file}")
-             selected_file
+              recipient_file_option = option :recipients_file
+              recipient_file = if recipient_file_option.nil?
+                                 debug('Searching for any hiera-eyaml-gpg.recipients files in path')
+                                 find_recipient_file
+                               else
+                                 debug('Using --recipients-file option')
+                                 Pathname.new(recipient_file_option)
+                               end
+
+              return [] if recipient_file.nil?
+
+              recipient_file.readlines.map do |line|
+                line.strip unless line.start_with?('#') || line.strip.empty?
+              end.compact
+            end
+
+            def find_recipient_file
+              # if we are editing a file, look for a hiera-eyaml-gpg.recipients file
+              filename = case Eyaml::Options[:source]
+                         when :file
+                           Eyaml::Options[:file]
+                         when :eyaml
+                           Eyaml::Options[:eyaml]
+                         end
+
+              return if filename.nil?
+
+              root = Pathname.new(filename).realpath.dirname
+              selected_file = nil
+              root.descend do |path|
+                potential_file = path.join('hiera-eyaml-gpg.recipients')
+                selected_file = potential_file if potential_file.exist?
+              end
+              debug("Using file at #{selected_file}")
+              selected_file
+            end
           end
         end
       end
